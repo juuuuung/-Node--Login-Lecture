7cf4af5b58167d40402539ce4c58afc01bea9323
juuuuung <pandanam987@gmail.com>
2022-10-28 22:45:18 +0900

    fetch() header, method, json.stringify, body

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -8,4 +8,14 @@ loginBtn.addEventListener("click", login);
 
 function login() {
   const req = { id: id.value, pw: pw.value };
+
+  console.log(req);
+  console.log(JSON.stringify(req));
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  });
 }
