cac9bc1c7c296efa1ac36c494e73edb777bc20dc
juuuuung <pandanam987@gmail.com>
2022-10-28 22:47:03 +0900

    불필요 코드 삭제

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -9,8 +9,6 @@ loginBtn.addEventListener("click", login);
 function login() {
   const req = { id: id.value, pw: pw.value };
 
-  console.log(req);
-  console.log(JSON.stringify(req));
   fetch("/login", {
     method: "POST",
     headers: {
